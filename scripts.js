a42c81734db6ee1ae1c5ca99dbf146c5d54d85e7
Alena Nikulina <alenanikulina0@gmail.com>
2020-06-17 17:30:05 -0400

    added a scrub func so video changes the timing depending on where the user clicks on the progress bar

--- scripts.js
+++ scripts.js
@@ -45,6 +45,12 @@ function handleProgress() {
     progressBar.style.flexBasis = `${percent}%`
 }
 
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+    // console.log(e)
+    video.currentTime = scrubTime
+}
+
 // Step 3 - hook up the event listeners
 video.addEventListener('click', togglePlay)
 video.addEventListener('play', updateButton)
@@ -57,3 +63,5 @@ skipButtons.forEach(button => button.addEventListener('click', skip))
 
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+
+progress.addEventListener('click', scrub)
